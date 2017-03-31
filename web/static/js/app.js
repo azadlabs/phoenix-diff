213b61bfedc3e14b332622d784c6afae1b4c4450
Navin Peiris <navinpeiris@gmail.com>
2017-03-31 07:29:47 +0530

    Configure angular location provider to not show hash in URL

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -5,4 +5,7 @@ import DiffController from './controllers/diff-controller';
 angular.module('phoenixDiff', [
   "ngAnimate"
 ])
+  .config(['$locationProvider', $locationProvider => {
+    $locationProvider.html5Mode({enabled: true, requireBase: true, rewriteLinks: false});
+  }])
   .controller("DiffController", DiffController);
